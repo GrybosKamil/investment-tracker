29c70d6a717005d0ab022b65ea6e0391c7ce055f
Grybos Kamil <grybos.kamil@gmail.com>
2025-02-02 19:34:22 +0100

    Add cors.

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import { apiRoutes } from "./routes/apiRoutes.js";
 
@@ -14,10 +15,16 @@ const mongoURI = process.env.MONGODB_URI;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json());
-app.use("/api", apiRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  })
+);
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../client/dist")));
+app.use("/api", apiRoutes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
