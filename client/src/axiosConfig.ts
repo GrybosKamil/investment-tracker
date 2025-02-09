1e773015e57b7a4e7bac016f7902571dda5206eb
Grybos Kamil <grybos.kamil@gmail.com>
2025-02-09 19:34:35 +0100

    Clear.

--- client/src/axiosConfig.ts
+++ client/src/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosInstance =
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   process.env.NODE_ENV === "development"
     ? axios.create({
         baseURL: "http://localhost:3000",
