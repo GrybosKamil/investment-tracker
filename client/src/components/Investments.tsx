fad59dfcef9f67d125924b0e1f197ec73d20e2e4
Grybos Kamil <grybos.kamil@gmail.com>
2025-02-18 01:51:52 +0100

    Add filtering of investments.

--- client/src/components/Investments.tsx
+++ client/src/components/Investments.tsx
@@ -1,7 +1,8 @@
 import { Button } from "primereact/button";
-import { Panel } from "primereact/panel";
-import { DataTable } from "primereact/datatable";
+import { Checkbox } from "primereact/checkbox";
 import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Panel } from "primereact/panel";
 import { useState } from "react";
 import { ExportInvestments } from "./ExportInvestments";
 import { ImportInvestments } from "./ImportInvestments";
@@ -14,10 +15,14 @@ import { useInvestments, useMutateInvestments } from "./useInvestments";
 export function Investments() {
   const [showList, setShowList] = useState<boolean>(false);
   const [confirmDeleteId, setConfirmDeleteId] = useState<string | null>(null);
-  const [selectedInvestmentId, setSelectedInvestmentId] = useState<string | null>(null); // State for selected investment
+  const [selectedInvestmentId, setSelectedInvestmentId] = useState<
+    string | null
+  >(null);
+  const [visibleColumns, setVisibleColumns] = useState<Record<string, boolean>>(
+    {},
+  );
 
   const { deleteMutation } = useMutateInvestments();
-
   const { isLoading, isError, data } = useInvestments();
 
   const handleDeleteClick = (id: string) => {
@@ -31,15 +36,22 @@ export function Investments() {
   const handleConfirmDelete = (id: string) => {
     deleteMutation.mutate(id);
     setConfirmDeleteId(null);
-    setSelectedInvestmentId(null); 
+    setSelectedInvestmentId(null);
   };
 
   const handleInvestmentClick = (id: string) => {
     if (selectedInvestmentId !== id) {
       setSelectedInvestmentId(id);
     } else {
-      setSelectedInvestmentId(null); 
+      setSelectedInvestmentId(null);
     }
+  };
+
+  const handleColumnVisibilityChange = (typeId: string) => {
+    setVisibleColumns((prev) => ({
+      ...prev,
+      [typeId]: !prev[typeId],
+    }));
   };
 
   if (isLoading) {
@@ -53,25 +65,33 @@ export function Investments() {
   const investments: Investment[] = data.investments;
   const investmentTypes: InvestmentType[] = data.investmentTypes;
 
-  const groupedInvestments = investments.reduce((acc, investment) => {
-    const date = investment.date.toISOString().split('T')[0];
-    if (!acc[date]) {
-      acc[date] = {};
-    }
-    if (!acc[date][investment.type]) {
-      acc[date][investment.type] = [];
-    }
-    acc[date][investment.type].push(investment);
-    return acc;
-  }, {} as Record<string, Record<string, Investment[]>>);
-
-  const investmentList = Object.entries(groupedInvestments).map(([date, investmentsByType]) => ({
-    date,
-    ...investmentTypes.reduce((acc, type) => {
-      acc[type._id] = investmentsByType[type._id] || [];
+  const groupedInvestments = investments.reduce(
+    (acc, investment) => {
+      const date = investment.date.toISOString().split("T")[0];
+      if (!acc[date]) {
+        acc[date] = {};
+      }
+      if (!acc[date][investment.type]) {
+        acc[date][investment.type] = [];
+      }
+      acc[date][investment.type].push(investment);
       return acc;
-    }, {} as Record<string, Investment[]>)
-  }));
+    },
+    {} as Record<string, Record<string, Investment[]>>,
+  );
+
+  const investmentList = Object.entries(groupedInvestments).map(
+    ([date, investmentsByType]) => ({
+      date,
+      ...investmentTypes.reduce(
+        (acc, type) => {
+          acc[type._id] = investmentsByType[type._id] || [];
+          return acc;
+        },
+        {} as Record<string, Investment[]>,
+      ),
+    }),
+  );
 
   return (
     <div>
@@ -99,56 +119,76 @@ export function Investments() {
       )}
 
       {showList && (
-        <DataTable value={investmentList}>
-          <Column field="date" header="Date" />
-          {investmentTypes.map((type:InvestmentType) => (
-            <Column
-              key={type._id}
-              field={type._id}
-              header={type.name}
-              body={(rowData:Record<string, Investment[]>) => (
-                <>
-                  {rowData[type._id].map(({ _id, value }:Investment) => (
-                    <div key={_id}>
-                      <span
-                        onClick={() => handleInvestmentClick(_id)}
-                        style={{ marginLeft: '0.5rem', cursor: 'pointer' }}
-                      >
-                        {value}
-                      </span>
-                      {selectedInvestmentId === _id && (
-                        <Panel header="Investment Details">
-                          <div>
-                            {confirmDeleteId === _id ? (
-                              <>
-                                <Button
-                                  onClick={() => handleConfirmDelete(_id)}
-                                  label="Confirm"
-                                  severity="danger"
-                                />
-                                <Button
-                                  onClick={handleCancelDelete}
-                                  label="Cancel"
-                                  severity="secondary"
-                                />
-                              </>
-                            ) : (
-                              <Button
-                                onClick={() => handleDeleteClick(_id)}
-                                label="Delete"
-                                severity="danger"
-                              />
+        <>
+          <div>
+            {investmentTypes.map((type) => (
+              <div key={type._id}>
+                <Checkbox
+                  inputId={type._id}
+                  checked={visibleColumns[type._id] !== false}
+                  onChange={() => handleColumnVisibilityChange(type._id)}
+                />
+                <label htmlFor={type._id}>{type.name}</label>
+              </div>
+            ))}
+          </div>
+          <DataTable value={investmentList}>
+            <Column field="date" header="Date" />
+            {investmentTypes.map(
+              (type: InvestmentType) =>
+                visibleColumns[type._id] !== false && (
+                  <Column
+                    key={type._id}
+                    field={type._id}
+                    header={type.name}
+                    body={(rowData: Record<string, Investment[]>) => (
+                      <>
+                        {rowData[type._id].map(({ _id, value }: Investment) => (
+                          <div key={_id}>
+                            <span
+                              onClick={() => handleInvestmentClick(_id)}
+                              style={{
+                                marginLeft: "0.5rem",
+                                cursor: "pointer",
+                              }}
+                            >
+                              {value}
+                            </span>
+                            {selectedInvestmentId === _id && (
+                              <Panel header="Investment Actions">
+                                <div>
+                                  {confirmDeleteId === _id ? (
+                                    <>
+                                      <Button
+                                        onClick={() => handleConfirmDelete(_id)}
+                                        label="Confirm"
+                                        severity="danger"
+                                      />
+                                      <Button
+                                        onClick={handleCancelDelete}
+                                        label="Cancel"
+                                        severity="secondary"
+                                      />
+                                    </>
+                                  ) : (
+                                    <Button
+                                      onClick={() => handleDeleteClick(_id)}
+                                      label="Delete"
+                                      severity="danger"
+                                    />
+                                  )}
+                                </div>
+                              </Panel>
                             )}
                           </div>
-                        </Panel>
-                      )}
-                    </div>
-                  ))}
-                </>
-              )}
-            />
-          ))}
-        </DataTable>
+                        ))}
+                      </>
+                    )}
+                  />
+                ),
+            )}
+          </DataTable>
+        </>
       )}
     </div>
   );
