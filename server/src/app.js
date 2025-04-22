26dd27e9a8e9c0ac1ba7ea70864fb439fb528e12
Grybos Kamil <grybos.kamil@gmail.com>
2025-04-23 00:05:45 +0200

    Update get wildcard to express 5.

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 app.use("/api", apiRoutes);
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
